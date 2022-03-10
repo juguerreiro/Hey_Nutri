4d0f673aa027f4cc2411c69509ffb03b3d3085d7
Mariana Lima <marianalima.amb@gmail.com>
2022-03-10 17:18:48 -0300

    created 2 migrations and added age and name to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_180556) do
+ActiveRecord::Schema.define(version: 2022_03_10_201354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2022_03_10_180556) do
     t.string "comorbidity"
     t.float "height"
     t.float "weight"
+    t.string "name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
