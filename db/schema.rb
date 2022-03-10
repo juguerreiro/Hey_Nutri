c3807d1511b6b23db99f9cfcc82284a7cf3cc78c
Mariana Lima <marianalima.amb@gmail.com>
2022-03-10 17:42:40 -0300

    added gender to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_201354) do
+ActiveRecord::Schema.define(version: 2022_03_10_203810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_03_10_201354) do
     t.float "weight"
     t.string "name"
     t.integer "age"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
