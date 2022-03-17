136456b0061a8c7d4788bd25e4b8d232ccb75f6b
Mariana Lima <marianalima.amb@gmail.com>
2022-03-17 12:28:08 -0300

    profile completed and working on frontend

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
   has_many :meals
 
   def bmi     
-   return bmi = (self.weight/ (self.height* self.height))*10000
+   return bmi = (weight/ (height* height))*10000
   end
 
   def bmr 
-    if self.gender == "male"
-    return  88.362 + (13.397 * self.weight) + (4.799 * self.height) - (5.677 * self.age)
+    if gender == "male"
+    return  88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
     else 
-      return   447.593 + (9.247 * self.weight) + (3.098 * self.height) - (4.330 * self.age)
+      return   447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
     end
   end
 end
