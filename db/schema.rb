5b341332b7994e441c8de2c9da7e9627f5a70c12
Julia Guerreiro <juguerreiro123@gmail.com>
2022-03-17 17:17:03 -0300

    working on map

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_153834) do
+ActiveRecord::Schema.define(version: 2022_03_17_200917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_03_17_153834) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "phone"
   end
 
   create_table "fibras", force: :cascade do |t|
