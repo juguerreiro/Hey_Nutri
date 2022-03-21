a1b8a73f483342d554013b54d8b32b0bbbb0c4b8
Julia Guerreiro <juguerreiro123@gmail.com>
2022-03-21 17:57:22 -0300

    og image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     meal_path(current_user)
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
